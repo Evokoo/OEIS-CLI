da95de7a23b03a810e11dbaddcce5bd8d7ecb0cd
Evokoo <evanjohncooper@gmail.com>
2024-05-09 01:11:14 +1000

    Fixed bundling issue / node compatibility

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
-import resolve from "@rollup/plugin-node-resolve";
-import typescript from "@rollup/plugin-typescript";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import esbuild from "rollup-plugin-esbuild";
 import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
 
@@ -7,7 +7,12 @@ export default {
 	input: "src/index.ts", // Your main CLI script
 	output: {
 		file: "build/oeis-cli.js", // Output file name
-		format: "cjs", // CommonJS format for Node.js execution
+		format: "es",
 	},
-	plugins: [typescript(), commonjs(), resolve(), json()],
+	plugins: [
+		nodeResolve({ preferBuiltins: true, exportConditions: ["node"] }),
+		commonjs(),
+		json(),
+		esbuild({ minify: true }),
+	],
 };
